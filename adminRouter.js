9f8534c8ac9a130f1db2fdd32629233dbd4c08f8
Adarsh <adarshshanu3@gmail.com>
2023-12-24 22:28:07 +0530

    updations

--- adminRouter.js
+++ adminRouter.js
@@ -3,149 +3,16 @@ const adminRouter = express.Router()
 const collection = require('./mongodb')
 const controller = require('./controller/adminController')
 
-const credential = {
-    username: 'Adarsh',
-    password: 'adarsh'
-}
 
 adminRouter.get('/admin', controller.adminLoginPage);
 adminRouter.post('/logi', controller.adminLoginPost);
-
-adminRouter.get('/home', async (req, res) => {
-    try {
-        if (req.session.user) {
-            const data = await collection.find();
-            res.render("adminpnl", {
-                title: 'Home page',
-                data: data,
-            })
-        } else {
-            res.redirect('/route/admin')
-        }
-    } catch (error) {
-        console.log(error)
-    }
-})
-
-adminRouter.get('/add', (req, res) => {
-    try {
-        res.render('add_users', { title: 'Add users' })
-    } catch (error) {
-        console.log(error)
-    }
-})
-
-
-adminRouter.post('/add', async (req, res) => {
-    try {
-        if (req.session.logi) {
-            const data = {
-                name: req.body.name,
-                email: req.body.email,
-                phone: req.body.phone,
-                password: req.body.password
-            }
-            console.log(req.body.name)
-            await collection.insertMany([data])
-            res.redirect('/route/home')
-        } else {
-            res.redirect('/route/admin')
-        }
-    } catch (error) {
-        console.log(error)
-    }
-})
-
-adminRouter.get('/admin_logout', (req, res) => {
-    try {
-        req.session.destroy((err) => {
-            if (err) {
-                res.send(err)
-                console.log(err)
-            } else {
-                res.redirect('/route/admin')
-            }
-
-        })
-    } catch (error) {
-        console.log(error)
-    }
-})
-
-adminRouter.get('/edit/:id', async (req, res) => {
-    try {
-        let id = req.params.id
-        collection.findById(id, (err, user) => {
-            if (err) {
-                res.redirect('/home')
-            } else {
-                if (user == null) {
-                    res.redirect('/home')
-                } else {
-                    res.render('edit_users', { title: 'Edit User', collection: user })
-                }
-            }
-        })
-    } catch (error) {
-        console.log(error)
-    }
-})
-
-adminRouter.post('/update/:id', (req, res) => {
-    try {
-        let id = req.params.id
-        collection.findByIdAndUpdate(id, {
-            name: req.body.name,
-            email: req.body.email,
-            phone: req.body.phone,
-            password: req.body.password
-        },
-            (err, result) => {
-                if (err) {
-                    res.send(err)
-                } else {
-                    res.redirect('/route/home')
-                }
-            }
-        )
-    } catch (error) {
-        console.log(error)
-    }
-})
-
-adminRouter.get('/delete/:id', (req, res) => {
-    try {
-        let id = req.params.id
-        collection.findByIdAndRemove(id, (err, result) => {
-            if (err) {
-                res.send(err)
-            } else {
-                res.redirect('/route/home')
-            }
-        })
-    } catch (error) {
-        console.log(error)
-    }
-})
-
-adminRouter.post('/search', async (req, res) => {
-    try {
-        let searchTerm = req.body.searchTerm
-        var emai = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/
-        let searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z0-9]/g, "");
-        let searchemail = searchTerm.replace(emai)
-
-        const person = await collection.find({
-            $or: [
-                { name: { $regex: new RegExp(searchNoSpecialChar, "i") } },
-                { email: { $regex: new RegExp(searchemail, "i") } },
-            ]
-        })
-        res.render('search', { person, title: 'search results' })
-
-    } catch (error) {
-        console.log(err)
-    }
-})
+adminRouter.get('/home',controller.homePage);
+adminRouter.get('/add',controller.addUserPage)
+adminRouter.post('/add',controller.addUserPost)
+adminRouter.get('/admin_logout', controller.adminLogout);
+adminRouter.get('/edit/:id', controller.editUserPage);
+adminRouter.post('/update/:id', controller.updatePost);
+adminRouter.delete('/delete/:id', controller.deleteUser);
+adminRouter.post('/search', controller.Search);
 
 module.exports = adminRouter
--- controller/adminController.js
+++ controller/adminController.js
@@ -1,3 +1,9 @@
+const collection = require("../mongodb")
+
+const credential = {
+    username: 'Adarsh',
+    password: 'adarsh'
+}
 
 
 function adminLoginPage(req, res) {
@@ -5,7 +11,7 @@ function adminLoginPage(req, res) {
         if (req.session.logined) {
             res.redirect('/route/home')
         } else {
-            res.render('adminlog',{title:"admin Login"})
+            res.render('adminlog', { title: "admin Login" })
         }
     } catch (error) {
         console.log(error)
@@ -17,7 +23,7 @@ function adminLoginPost(req, res) {
         if (credential.username == req.body.name && credential.password == req.body.password) {
             req.session.user = req.body.name
             req.session.logined = true
-            req.session.logi=true
+            req.session.logi = true
             res.redirect('/route/home')
             console.log(req.session)
         } else {
@@ -28,7 +34,144 @@ function adminLoginPost(req, res) {
     }
 }
 
-module.exports={
+async function homePage(req, res) {
+    try {
+        if (req.session.user) {
+            const data = await collection.find();
+            res.render("adminpnl", {
+                title: 'Home page',
+                data: data,
+            })
+        } else {
+            res.redirect('/route/admin')
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+function addUserPage(req, res) {
+    try {
+        res.render('add_users', { title: 'Add users' })
+    } catch (error) {
+        console.log(error)
+    }
+}
+async function addUserPost(req, res) {
+    try {
+        if (req.session.logi) {
+            const data = {
+                name: req.body.name,
+                email: req.body.email,
+                phone: req.body.phone,
+                password: req.body.password
+            }
+            console.log(req.body.name)
+            await collection.insertMany([data])
+            res.redirect('/route/home')
+        } else {
+            res.redirect('/route/admin')
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+function adminLogout(req, res) {
+    try {
+        req.session.destroy((err) => {
+            if (err) {
+                res.send(err)
+                console.log(err)
+            } else {
+                res.redirect('/route/admin')
+            }
+
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+async function editUserPage(req, res) {
+    try {
+        let id = req.params.id
+        collection.findById(id, (err, user) => {
+            if (err) {
+                res.redirect('/home')
+            } else {
+                if (user == null) {
+                    res.redirect('/home')
+                } else {
+                    res.render('edit_users', { title: 'Edit User', collection: user })
+                }
+            }
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+function updatePost(req, res) {
+    try {
+        let id = req.params.id
+        collection.findByIdAndUpdate(id, {
+            name: req.body.name,
+            email: req.body.email,
+            phone: req.body.phone,
+            password: req.body.password
+        },
+            (err, result) => {
+                if (err) {
+                    res.send(err)
+                } else {
+                    res.redirect('/route/home')
+                }
+            }
+        )
+    } catch (error) {
+        console.log(error)
+    }
+}
+function deleteUser(req, res) {
+    try {
+        let id = req.params.id
+        collection.findByIdAndRemove(id, (err, result) => {
+            if (err) {
+                res.send(err)
+            } else {
+                res.redirect('/route/home')
+            }
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+async function Search(req, res) {
+    try {
+        let searchTerm = req.body.searchTerm
+        var emai = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/
+        let searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z0-9]/g, "");
+        let searchemail = searchTerm.replace(emai)
+
+        const person = await collection.find({
+            $or: [
+                { name: { $regex: new RegExp(searchNoSpecialChar, "i") } },
+                { email: { $regex: new RegExp(searchemail, "i") } },
+            ]
+        })
+        res.render('search', { person, title: 'search results' })
+
+    } catch (error) {
+        console.log(err)
+    }
+}
+
+module.exports = {
     adminLoginPage,
-    adminLoginPost
+    adminLoginPost,
+    homePage,
+    addUserPage,
+    addUserPost,
+    adminLogout,
+    editUserPage,
+    updatePost,
+    deleteUser,
+    Search
 }
