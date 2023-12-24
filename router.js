5c3b1142d93ea4f788b8936ab6cc9ada49209598
Adarsh <adarshshanu3@gmail.com>
2023-12-24 21:04:28 +0530

    Modified router.js

--- router.js
+++ router.js
@@ -3,9 +3,6 @@ var router = express.Router()
 const collection = require('./mongodb')
 
 
-// router.get('/login',(req,res)=>{
-//     res.redirect('/')
-// })
 
 router.post('/login', async (req, res) => {
     try {
@@ -13,15 +10,12 @@ router.post('/login', async (req, res) => {
         if (check.password === req.body.password) {
             req.session.loginee = true
             req.session.nam = req.body.name
-            // res.render('dashboard',{nam:req.session.nam})
             res.redirect('/route/dashboard')
         } else {
             res.render('base', { incorr: 'incorrect password...' })
-            // res.redirect('/')
         }
     }
     catch {
-        // res.render('base',{incorr:'Wrong Details!'})
         res.redirect('/')
     }
 })
@@ -29,43 +23,38 @@ router.get('/signup', (req, res) => {
     res.render('deta')
 })
 router.post('/signup', async (req, res) => {
-    const data = {
-        name: req.body.name,
-        email: req.body.email,
-        phone: req.body.phone,
-        password: req.body.password
-    }
+    try {
+        const data = {
+            name: req.body.name,
+            email: req.body.email,
+            phone: req.body.phone,
+            password: req.body.password
+        }
         await collection.insertMany([data])
         res.redirect('/')
 
-
+    } catch (error) {
+        console.log(error)
+    }
 })
 router.get('/dashboard', (req, res) => {
-    if (req.session.nam) {
-        res.render('dashboard', { nam: req.session.nam,title:'Dashboard' })
-    } else {
-        res.redirect('/')
+    try {
+        if (req.session.nam) {
+            res.render('dashboard', { nam: req.session.nam, title: 'Dashboard' })
+        } else {
+            res.redirect('/')
+        }
+    } catch (error) {
+        console.log(error)
     }
-
 })
 router.get('/logout', (req, res) => {
-    req.session.destroy()
-    res.redirect('/')
-    // try{
-    //     req.session.destroy((err) => {
-    //         if (err) {
-    //             console.log(err)
-    //             res.send("Error")
-    //         } else {
-    //             res.redirect('/')
-    //             console.log(req.session)
-    //         }
-    //     })
-    // }
-    // catch{
-    //     res.redirect('/')
-    // }
-
+    try {
+        req.session.destroy()
+        res.redirect('/')
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 
